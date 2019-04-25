2c75bf65ac101bbb178ae976e516387a74a16e7f
mbisht <mbisht@tirebuyer.com>
2019-04-24 17:01:59 -0700

    Dataset importer module

---
+++ constants/project_properties.py
@@ -0,0 +1,3 @@
+
+#######Dataset URL
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
---
+++ import/datasetimporter.py
@@ -0,0 +1,24 @@
+import os
+from constants import project_properties as prop
+import tarfile
+from six.moves import urllib
+
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = prop.DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+
+    # unzip and retrieve csv file
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+if __name__ == '__main__':
+    fetch_housing_data()
+
